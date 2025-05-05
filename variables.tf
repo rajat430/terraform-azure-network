ffbd3bc2be334852212dd5287ea62c4cfad0fbd4
rajat430 <47021948+rajat430@users.noreply.github.com>
2025-05-05 14:50:14 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "vnet_resource_group" {
+  type = string
+  description = "Provide vnet rg"
+
+}
+
+variable "vnet_name" {
+ type=string
+ description = "virtual network name"
+}
+
+variable "vnet_address_space" {
+  
+  type=list(string)
+  description = "list of vnet address spaces"
+}
+
+variable "dns_servers" {
+  type=list(string)
+  description = "list of dns servers"
+}
+
+variable "subnet_details" {
+  type=map(object({
+    name = string
+    address_prefixes = list(string)
+    nsg_name = string
+    nsg_rules = map(object({
+        name = string
+    }))
+  }))
+}
