dfc141070f9ccc37dd8d41b7e46db0384eb8af10
rajat430 <47021948+rajat430@users.noreply.github.com>
2025-05-05 14:49:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+
+# data "azurerm_resource_group" "example" {
+#     name = var.vnet_resource_group
+# }
+
+data "azurerm_resource_group" "vnet_rg" {
+ name =  var.vnet_resource_group
+}
+
+resource "azurerm_network_security_group" "example" {
+  for_each = var.subnet_details
+  name                = each.value.nsg_name
+  location            = data.azurerm_resource_group.vnet_rg.location
+  resource_group_name = data.azurerm_resource_group.vnet_rg.name
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  location            = data.azurerm_resource_group.vnet_rg.location
+  resource_group_name = data.azurerm_resource_group.vnet_rg.name
+  address_space       = var.vnet_address_space
+  dns_servers         = var.dns_servers
+
+  dynamic "subnet" {
+    for_each = var.subnet_details 
+    content {
+      name = subnet.value.name
+      address_prefixes = subnet.value.address_prefixes
+      security_group = azurerm_network_security_group.example[subnet.key].id
+    }
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
